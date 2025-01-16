777777e03edcc0fde0b4e3e2cc240e18d34721b2
Ant-DeLaT <186017554+Ant-DeLaT@users.noreply.github.com>
2025-01-16 12:07:01 +0100

    FixedDatabase

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -1,5 +1,5 @@
 <?php
-
+// MUY IMPORTANTE
 namespace Config;
 
 use CodeIgniter\Database\Config;
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'test',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
